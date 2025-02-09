e0d4350067da3e8fa7aaaaccebbccb46f8d047be
Nukesh <nukeshpaudel99@gmail.com>
2025-02-09 12:50:58 +0545

    changes committed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  resolve: {
+    alias: {
+      "@": "/src", // Optional alias for cleaner imports
+    },
+  },
+});
